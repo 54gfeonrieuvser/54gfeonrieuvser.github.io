8de4129628567c11cb888ddc2ac03aca2fea6190
54gfeonrieuvser <b03902030@ntu.edu.tw>
2020-10-01 20:17:21 +0800

    debug event and clean the message

--- Side_Project/FNM_js_prac/gallery.js
+++ Side_Project/FNM_js_prac/gallery.js
@@ -1,21 +1,14 @@
 let mydog = document.getElementsByClassName("gallery-img");
 function next(){
-    console.log(document.getElementsByClassName("next")[0]);
-    console.log(document.getElementsByClassName("prev")[0]);
-    console.log("gap");
     document.getElementsByClassName("prev")[0].disabled = false;
     const temporaryimg = document.getElementsByClassName("active");
     const activate = document.getElementsByClassName("prev");
     for(let i = 0; i <mydog.length; i++){
         if(mydog[i] == temporaryimg[0]){
             if(i === (mydog.length - 2)){
-                // let att = document.createAttribute("disabled");
-                // att.value = "true";
                 document.getElementsByClassName("next")[0].disabled = true;
             }
-            // if(i === (mydog.length -1)){
-            //    return;
-            // }
+
             mydog[i+1].classList.add("active");
             mydog[i].classList.remove("active");
             return;
@@ -23,9 +16,6 @@ function next(){
     }
 }
 function prev(){
-    console.log(document.getElementsByClassName("next")[0]);
-    console.log(document.getElementsByClassName("prev")[0]);
-    console.log("gap");
     document.getElementsByClassName("next")[0].disabled = false;
     const temporaryimg = document.getElementsByClassName("active");
     for(let i = 0; i <mydog.length; i++){
@@ -33,9 +23,6 @@ function prev(){
             if(i === 1){
                 document.getElementsByClassName("prev")[0].disabled = true;
             }
-            // if(i === 0){
-            //     return;
-            // }
             mydog[i-1].classList.add("active");
             mydog[i].classList.remove("active");
             return;
